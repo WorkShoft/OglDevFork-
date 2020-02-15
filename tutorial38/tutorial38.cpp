1dc7b02ee967d2e0e0b85d842d7f751b281ed669
Mikel <mikel.losada@gmail.com>
2020-02-15 16:02:27 +0100

    Make some default camera settings corrections

--- tutorial38/tutorial38.cpp
+++ tutorial38/tutorial38.cpp
@@ -73,7 +73,7 @@ public:
 
     bool Init()
     {
-        Vector3f Pos(0.0f, 3.0f, -1.0f);
+        Vector3f Pos(1.0f, 0.35f, 4.0f);
         Vector3f Target(0.0f, 0.0f, 1.0f);
         Vector3f Up(0.0, 1.0f, 0.0f);
 
@@ -142,7 +142,8 @@ public:
              
         Vector3f Pos(m_position);
         p.WorldPos(Pos);        
-        p.Rotate(270.0f, 180.0f, 0.0f);       
+        //p.Rotate(270.0f, 180.0f, 0.0f);
+	p.Rotate(270.0f, 270.0f, 10.0f);
         m_pEffect->SetWVP(p.GetWVPTrans());
         m_pEffect->SetWorldMatrix(p.GetWorldTrans());            
 
