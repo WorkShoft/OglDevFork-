b26b768b19b88b09229e04b3858b8c93d16097d2
Mikel <mikel.losada@gmail.com>
2020-03-01 12:42:50 +0100

    Update Assimp to newest version

---
+++ tutorial38/CMakeFiles/tutorial38.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tutorial38"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/tutorial38.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
